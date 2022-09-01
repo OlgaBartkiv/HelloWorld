78807eb2bf1963fd9d102ccc68e35f31a9fff63a
OlgaBartkiv <olga.bartkiv@ratesetter.com>
2022-09-01 13:25:38 +0300

    added Cycles class

---
+++ Cycles.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Cycles
+    {
+        public void ForLoop()
+        {
+            for (int a=0; a<10; a++)
+            {
+                Console.WriteLine($"For loop iteration number: {a+1}");
+            }
+        }
+
+        public void WhileLoop()
+        {
+            int b=0;
+            while (b<10)
+            {
+                Console.WriteLine($"While loop iteration number: {b+1}");
+                b++;
+            }
+        }
+
+        public void DoWhileLoop()
+        {
+            int c = 0;
+            do
+            {
+                Console.WriteLine($"DoWhile loop iteration number: {c+1}");
+                c++;
+            }
+            while (c < 10);
+        }
+
+        /// <summary>
+        /// Recursive method of getting factorial number with exit condition
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public int FactorialWithExit(int n)
+        {
+            if (n == 0) // exit condition
+                return 1;
+            else
+                return n * FactorialWithExit(n - 1);
+        }
+
+        /// <summary>
+        /// Recursive method of getting factorial number with no exit condition - Stack Overflow Exception is thrown
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public int FactorialNoExit(int n)
+        {
+            return n * FactorialNoExit(n - 1);
+        }
+
+        /// <summary>
+        /// Condition repeats indefinitely
+        /// </summary>
+        public void InfiniteLoopFor()
+        {
+            for(; ; )
+            {
+                Console.WriteLine("I love C#");
+            }
+        }
+
+        public void PrintingArrayUsingForeach()
+        {
+            char[] myArray = { 'O', 'l', 'g', 'a' };
+            foreach(char x in myArray)
+            {
+                Console.WriteLine(x);
+            }
+        }
+
+        public int ReverseNumber(int number)
+        {
+            int reverse = 0, remaining;
+            while (number!=0)
+            {
+                remaining = number % 10;
+                reverse = reverse * 10 + remaining;
+                number /= 10;
+            }
+            return reverse;
+        }
+
+        public void CheckIfPalindrome(int myNumber)
+        {
+            int myReverse = ReverseNumber(myNumber); // calling ReverseNumber method 
+            
+            if (myNumber == myReverse)
+                Console.WriteLine($"This is a palindrome: {myNumber}");
+            else
+                Console.WriteLine($"This is not a palindrome: {myNumber}");
+        }
+
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,17 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Cycles cycles = new Cycles();
+            cycles.ForLoop();
+            cycles.WhileLoop();
+            cycles.DoWhileLoop();
+            Console.WriteLine($"Factorial number is: {cycles.FactorialWithExit(5)}");
+            //Console.WriteLine($"Factorial number is: {cycles.FactorialNoExit(5)}");// Stack Overflow exception
+            //cycles.InfiniteLoopFor();// is executed indefinitely
+            cycles.PrintingArrayUsingForeach();
+            Console.WriteLine($"Reversed number: {cycles.ReverseNumber(753081)}");
+            cycles.CheckIfPalindrome(753357);
         }
     }
 }
