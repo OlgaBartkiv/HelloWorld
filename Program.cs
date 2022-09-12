9e3bc384dbf57c8755198f1d669a63019fd6416f
OlgaBartkiv <olga.bartkiv@ratesetter.com>
2022-09-12 14:52:08 +0300

    added Interface class

--- Program.cs
+++ Program.cs
@@ -7,6 +7,23 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            AeroPress aeroPress = new AeroPress();
+            aeroPress.BrewCoffee();
+            //aeroPress.GrindCoffee();// gives error since the method is not visible from AeroPress class
+            ((ICoffeeMaker)aeroPress).GrindCoffee(); // works as expected
+            //aeroPress.FoamMilk(); // gives error since the method is not visible from AeroPress class
+            ((ICoffeeMaker)aeroPress).FoamMilk(); // works as expected
+            DelonghiMagnifica delonghiMagnifica = new DelonghiMagnifica();
+            delonghiMagnifica.BrewCoffee();
+            delonghiMagnifica.FoamMilk();
+            //delonghiMagnifica.GrindCoffee(); // gives error since the method is not visible from AeroPress class
+            ((ICoffeeMaker)delonghiMagnifica).GrindCoffee(); // works as expected
+            BrevilleBarista brevilleBarista = new BrevilleBarista();
+            brevilleBarista.BrewCoffee();
+            brevilleBarista.GrindCoffee();
+            brevilleBarista.FoamMilk();
+
         }
     }
 }
