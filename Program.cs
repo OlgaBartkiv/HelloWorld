3e32585145fc704fbe97ba93da5c25ecc301a4ea
OlgaBartkiv <olga.bartkiv@ratesetter.com>
2022-09-12 13:56:38 +0300

    added Inheritance class

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
 
 namespace HelloWorld
 {
@@ -7,6 +9,28 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            B b = new B("Bob", 14);
+            Animal cow = new Animal("Masha", 700, 5);
+            Debug.WriteLine(cow);
+            cow.Eat("grass", 700);
+            cow.Drink("water");
+            cow.Sleep(10);
+            cow.Poop();
+            cow.Sound("Mooooooo");
+            Dog dog = new Dog("Chester", 15, 8);
+            dog.Bark();
+            dog.Sleep(default);
+            dog.Chew("sock");
+            Cat cat = new Cat("Musya", 5, 4);
+            cat.Meow();
+            cat.Sleep(default);
+            cat.Scratch("sofa");
+
+
+
+
+
         }
     }
 }
