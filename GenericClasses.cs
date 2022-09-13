944c78d26bebc1f6ff6482dac99d006ba1666dff
OlgaBartkiv <olga.bartkiv@ratesetter.com>
2022-09-13 18:27:20 +0300

    added GenericClasses class

---
+++ GenericClasses.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorld
+{
+    public class Drink : IFood
+    {
+
+        public List<string> Ingredients { get; set; }
+        
+    }
+    public class Soup : IFood
+    {
+        public List<string> Ingredients { get; set; }
+        
+    }
+    public class MainCourse : IFood
+    {
+        public List<string> Ingredients { get; set; }
+           
+    }
+    public class Dessert : IFood
+    {
+        public List<string> Ingredients { get; set; }
+           
+    }
+
+    public interface IFood
+    {
+        public List<string> Ingredients { get; }
+    }
+
+    public class DairyFood<T> where T : IFood
+    {
+        public string DairyFree(T food)
+        {
+            Random rnd = new Random();
+            int num = rnd.Next(0, 3);
+            return food.Ingredients[num];
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace HelloWorld
 {
@@ -7,6 +9,59 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Drink drink = new Drink
+            {
+                Ingredients = new List<string>
+                {
+                    "coffee",
+                    "milk",
+                    "juice"
+                }
+            };
+            Soup soup = new Soup
+            {
+                Ingredients = new List<string>
+                {
+                    "broccoli",
+                    "carrot",
+                    "sour cream"
+                }
+            };
+            MainCourse mainCourse = new MainCourse
+            {
+                Ingredients = new List<string>
+                {
+                    "cheese",
+                    "salmon",
+                    "veel"
+                }
+            };
+            Dessert dessert = new Dessert
+            {
+                Ingredients = new List<string>
+                {
+                    "chocolate",
+                    "cream",
+                    "coconut"
+                }
+            };
+
+            DairyFood<Drink> dairyDrink = new DairyFood<Drink>();
+            string result1 = dairyDrink.DairyFree(drink);
+            Debug.WriteLine(result1);
+
+            DairyFood<Soup> dairySoup = new DairyFood<Soup>();
+            string result2 = dairySoup.DairyFree(soup);
+            Debug.WriteLine(result2);
+
+            DairyFood<MainCourse> dairyMainCourse = new DairyFood<MainCourse>();
+            string result3 = dairyMainCourse.DairyFree(mainCourse);
+            Debug.WriteLine(result3);
+
+            DairyFood<Dessert> dairyDessert = new DairyFood<Dessert>();
+            string result4 = dairyDessert.DairyFree(dessert);
+            Debug.WriteLine(result4);
         }
     }
 }
