7bb4c247321da559d52a65dc86cbd4c9b9de4e86
OlgaBartkiv <olga.bartkiv@ratesetter.com>
2022-09-13 13:42:49 +0300

    added Structures class

--- Program.cs
+++ Program.cs
@@ -7,6 +7,22 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            //struct can be initialized without using 'new' operator
+            CarStruct myCar;
+            myCar.make = "BMW";
+            myCar.mileage = 29000;
+            myCar.Print();
+            //struct initialized using 'new', in this case one of the parameters (e.g. mileage) can be skipped, a default value will be assigned for it
+            CarStruct anotherCar = new CarStruct()
+            {
+                make = "Audi"
+            };
+            anotherCar.Print();
+
+            //creating new object and calling method of class using class object
+            CarClass dreamCar = new CarClass();
+            dreamCar.CarDetails("Range Rover", 1000);
         }
     }
 }
