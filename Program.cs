e6ebb35e0215392fea268acd6b653169837fffd3
OlgaBartkiv <olga.bartkiv@ratesetter.com>
2022-09-16 17:04:52 +0300

    added RegexCases class

--- Program.cs
+++ Program.cs
@@ -7,6 +7,13 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            RegexCases regexCases = new RegexCases();
+            regexCases.ExtractDigitsFromText();
+            regexCases.ExtractLettersFromText();
+            regexCases.ExtractTextAfterDefinedSymbol();
+            regexCases.ExtractIPAddress();
+            regexCases.CreateFileAndExtractIPAddress();
         }
     }
 }
