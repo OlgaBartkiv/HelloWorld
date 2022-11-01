938dff6b66fa1ab8f4de2d49cef31eb8482f44f7
OlgaBartkiv <olga.bartkiv@ratesetter.com>
2022-11-01 18:47:43 +0200

    added Employee and EmployeeService classes

---
+++ Employee.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace HelloWorld
+{
+    public enum Achievement
+    {
+        [Description("Reorganized something to make it work better")]
+        Reorganized,
+        [Description("Identified a problem and solved it")]
+        Identified,
+        [Description("Come up with a new idea that improves things")]
+        NewIdea,
+        [Description("Developed new procedures")]
+        Developed,
+        [Description("Worked on special projects")]
+        Special,
+        [Description("Received awards")]
+        Awards,
+        [Description("Been complimented by manager or co-workers")]
+        Complimented
+    }
+    public class Employee
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Position { get; set; }
+
+        public Dictionary <int, Enum?> AchievementsByMonth { get; set; }
+
+        public bool IsAwarded;
+
+        public bool CanBeAwarded(int month)
+        {
+            var achievement = AchievementsByMonth[month];
+
+            if (achievement != null && !IsAwarded )
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+    
+}
--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using HelloWorld.Models;
 using System;
 using System.Diagnostics;
 using System.Collections.Generic;
+using HelloWorld.Services;
 
 namespace HelloWorld
 {
@@ -12,72 +13,72 @@ namespace HelloWorld
         {
             Console.WriteLine("Hello World!");
 
-            Drink drink = new Drink()
-            {
-                Ingredients = new List<string>
-                {
-                    "coffee",
-                    "milk",
-                    "juice"
-                }
-            };
-            Soup soup = new Soup
-            {
-                Ingredients = new List<string>
-                {
-                    "broccoli",
-                    "carrot",
-                    "sour cream"
-                }
-            };
-            MainCourse mainCourse = new MainCourse
-            {
-                Ingredients = new List<string>
-                {
-                    "cheese",
-                    "salmon",
-                    "veel"
-                }
-            };
-            Dessert dessert = new Dessert
-            {
-                Ingredients = new List<string>
-                {
-                    "chocolate",
-                    "cream",
-                    "coconut"
-                }
-            };
+            //Drink drink = new Drink()
+            //{
+            //    Ingredients = new List<string>
+            //    {
+            //        "coffee",
+            //        "milk",
+            //        "juice"
+            //    }
+            //};
+            //Soup soup = new Soup
+            //{
+            //    Ingredients = new List<string>
+            //    {
+            //        "broccoli",
+            //        "carrot",
+            //        "sour cream"
+            //    }
+            //};
+            //MainCourse mainCourse = new MainCourse
+            //{
+            //    Ingredients = new List<string>
+            //    {
+            //        "cheese",
+            //        "salmon",
+            //        "veel"
+            //    }
+            //};
+            //Dessert dessert = new Dessert
+            //{
+            //    Ingredients = new List<string>
+            //    {
+            //        "chocolate",
+            //        "cream",
+            //        "coconut"
+            //    }
+            //};
 
-            DairyFood<Drink> dairyDrink = new DairyFood<Drink>();
-            string result = dairyDrink.DairyFree(drink);
-            Debug.WriteLine(result);
+            //DairyFood<Drink> dairyDrink = new DairyFood<Drink>();
+            //string result = dairyDrink.DairyFree(drink);
+            //Debug.WriteLine(result);
 
-            DairyFood<Soup> dairySoup = new DairyFood<Soup>();
-            result = dairySoup.DairyFree(soup);
-            Debug.WriteLine(result);
+            //DairyFood<Soup> dairySoup = new DairyFood<Soup>();
+            //result = dairySoup.DairyFree(soup);
+            //Debug.WriteLine(result);
 
-            DairyFood<MainCourse> dairyMainCourse = new DairyFood<MainCourse>();
-            result = dairyMainCourse.DairyFree(mainCourse);
-            Debug.WriteLine(result);
+            //DairyFood<MainCourse> dairyMainCourse = new DairyFood<MainCourse>();
+            //result = dairyMainCourse.DairyFree(mainCourse);
+            //Debug.WriteLine(result);
 
-            DairyFood<Dessert> dairyDessert = new DairyFood<Dessert>();
-            result = dairyDessert.DairyFree(dessert);
-            Debug.WriteLine(result);
+            //DairyFood<Dessert> dairyDessert = new DairyFood<Dessert>();
+            //result = dairyDessert.DairyFree(dessert);
+            //Debug.WriteLine(result);
 
             /// 'new' constraint implementation
-            Lesson<Student> studentLesson = new Lesson<Student>(); // No error, because Student has parameterless constructor
+            /*Lesson<Student> studentLesson = new Lesson<Student>();*/ // No error, because Student has parameterless constructor
             /*Lesson<Teacher> teacherLesson = new Lesson<Teacher>();*/ // Error, because Teacher has constructor with parameter
 
-            StringCases stringCases = new StringCases();
-            stringCases.ParseStringToJson();
-            stringCases.ConcatenateStrings();
-            stringCases.TrimString();
-            stringCases.SplitStringAndPrintSeparate();
-            stringCases.SplitStringAndCleanEmptySpaces();
-            stringCases.ConvertingCharToUppercase();
-            stringCases.ConvertStringToUppercase();
-            stringCases.ParseValuesFromString();
+            //StringCases stringCases = new StringCases();
+            //stringCases.ParseStringToJson();
+            //stringCases.ConcatenateStrings();
+            //stringCases.TrimString();
+            //stringCases.SplitStringAndPrintSeparate();
+            //stringCases.SplitStringAndCleanEmptySpaces();
+            //stringCases.ConvertingCharToUppercase();
+            //stringCases.ConvertStringToUppercase();
+            //stringCases.ParseValuesFromString();
             //StringCases stringCases = new StringCases();
             //stringCases.ParseStringToJson();
             //stringCases.ConcatenateStrings();
@@ -159,29 +160,15 @@ namespace HelloWorld
             //conditionalMethods.UsingBitwiseOperators();
             //conditionalMethods.DefineAge();
 
-            //Collections collections = new Collections();
-            //collections.ListCollectionMethods();
-            //collections.LinkedListCollectionMethods();
-            //collections.HashSetCollectionMethods();
-            //collections.HashMapCollectionMethods();
-            //collections.IdentifyUniqueArrayElements();
-            //collections.PetsListMethods();
-            //collections.PoliticiansSortedListMethods();
-            //ValidParentheses validParentheses = new ValidParentheses();
-            //string input1 = "(3+5)*4/7+(7-3))";
-            //Debug.WriteLine(validParentheses.Valid(input1));
-            //object[] array = new object[5] { 1, 1.1111, "Sharad", 'c', 2.79769313486232E+3 };
-            //validParentheses.ValidArray(array);
-
-            ExceptionsCases exceptionsCases = new ExceptionsCases();
-            exceptionsCases.DivideInteger(55, 0);
-            exceptionsCases.DivideInteger("44", "0");
-            exceptionsCases.DivideInteger(12, "0");
-            exceptionsCases.DivideIntegerWithSwitch("33", "0");
-            exceptionsCases.DivideIntegerWithSwitchPattern("77", "0");
-            exceptionsCases.FindItemInCollection(4);
-            exceptionsCases.CreateFile();
-            exceptionsCases.DifferentLogLevels();
+            //ExceptionsCases exceptionsCases = new ExceptionsCases();
+            //exceptionsCases.DivideInteger(55, 0);
+            //exceptionsCases.DivideInteger("44", "0");
+            //exceptionsCases.DivideInteger(12, "0");
+            //exceptionsCases.DivideIntegerWithSwitch("33", "0");
+            //exceptionsCases.DivideIntegerWithSwitchPattern("77", "0");
+            //exceptionsCases.FindItemInCollection(4);
+            //exceptionsCases.CreateFile();
+            //exceptionsCases.DifferentLogLevels();
             //conditionalMethods.DefineAge();
 
             //Collections collections = new Collections();
@@ -200,28 +187,38 @@ namespace HelloWorld
             //conditionalMethods.DefineAge();
             //conditionalMethods.DefineAge();
 
-            //Collections collections = new Collections();
-            //collections.ListCollectionMethods();
-            //collections.LinkedListCollectionMethods();
-            //collections.HashSetCollectionMethods();
-            //collections.HashMapCollectionMethods();
-            //collections.IdentifyUniqueArrayElements();
-            //collections.PetsListMethods();
-            //collections.PoliticiansSortedListMethods();
-            //ValidParentheses validParentheses = new ValidParentheses();
-            //string input1 = "(3+5)*4/7+(7-3))";
-            //Debug.WriteLine(validParentheses.Valid(input1));
-            //object[] array = new object[5] { 1, 1.1111, "Sharad", 'c', 2.79769313486232E+3 };
-            //validParentheses.ValidArray(array);
+            //FileOperationsCases fileOperationsCases = new FileOperationsCases();
+            //HomeActivity myHomeActivity = new HomeActivity("Ironing", 2, "Saturday");
+            //string serialized = JsonConvert.SerializeObject(myHomeActivity, Formatting.Indented);
+            //Debug.WriteLine(serialized.DeserializeJson<HomeActivity>());
+            //Debug.WriteLine($"{myHomeActivity.Name}, {myHomeActivity.Duration}, {myHomeActivity.WeekDay}");
+            //fileOperationsCases.WritingJsonToFile(myHomeActivity);
+            //fileOperationsCases.ReadingFromFile();
 
-            FileOperationsCases fileOperationsCases = new FileOperationsCases();
-            HomeActivity myHomeActivity = new HomeActivity("Ironing", 2, "Saturday");
-            string serialized = JsonConvert.SerializeObject(myHomeActivity, Formatting.Indented);
-            Debug.WriteLine(serialized.DeserializeJson<HomeActivity>());
-            Debug.WriteLine($"{myHomeActivity.Name}, {myHomeActivity.Duration}, {myHomeActivity.WeekDay}");
-            fileOperationsCases.WritingJsonToFile(myHomeActivity);
-            fileOperationsCases.ReadingFromFile();
 
+            var dictionary1 = new Dictionary<int, Enum?> { { 1, null }, { 2, null }, { 3, null }, { 4, null }, { 5, null } };
+            var dictionary2 = new Dictionary<int, Enum?> { {1, null },{ 2, Achievement.Special }, { 3, null }, { 4, Achievement.Complimented }, { 5, Achievement.Identified } };
+            var dictionary3 = new Dictionary<int, Enum?> { { 1, Achievement.Awards }, { 2, Achievement.Reorganized }, { 3, Achievement.Complimented }, { 4, Achievement.Identified }, { 5, Achievement.Developed } };
+            var dictionary4 = new Dictionary<int, Enum?> { { 1, Achievement.Developed }, { 2, null }, {3, Achievement.Reorganized }, { 4, Achievement.Identified }, { 5, null } };
+            var dictionary5 = new Dictionary<int, Enum?> { { 1, Achievement.Complimented }, { 2, null }, { 3, null }, { 4, null }, { 5, null } };
+            List<Employee> meteorTeam = new List<Employee>()
+            {
+                new Employee() { FirstName = "Alex", LastName = "Bradbury", Position = "Developer", AchievementsByMonth = dictionary1, IsAwarded = false },
+                new Employee() { FirstName = "Olya", LastName = "Bartkiv", Position = "QA", AchievementsByMonth = dictionary2, IsAwarded = false},
+                new Employee() { FirstName = "Taras", LastName = "Onyshko", Position = "Developer", AchievementsByMonth = dictionary3, IsAwarded = true},
+                new Employee() { FirstName = "Davinder", LastName = "Kainth", Position = "Developer", AchievementsByMonth = dictionary4, IsAwarded = false},
+                new Employee() { FirstName = "Olha", LastName = "Bodnarchuk", Position = "QA", AchievementsByMonth = dictionary5, IsAwarded = false}
+            };
+
+            EmployeeService employeeService = new EmployeeService();
+            employeeService.Goat(1, meteorTeam);
+            employeeService.Goat(2, meteorTeam);
+            employeeService.Goat(3, meteorTeam);
+            employeeService.Goat(4, meteorTeam);
+            employeeService.Goat(5, meteorTeam);
+            
         }
+
+
     }
 }
