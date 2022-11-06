d781e85339a3775de888ba7ddd514eda3733ac9c
OlgaBartkiv <olga.bartkiv@ratesetter.com>
2022-11-06 19:53:36 +0200

    added NullableTypeCases class

--- Employee.cs
+++ Employee.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Text;
 
 namespace HelloWorld
@@ -22,6 +23,24 @@ namespace HelloWorld
         [Description("Been complimented by manager or co-workers")]
         Complimented
     }
+
+    public enum Title
+    {
+        [Description("Mr")]
+        Mr,
+        [Description("Mrs")]
+        Mrs,
+        [Description("Miss")]
+        Miss
+    }
+
+    public enum Level
+    {
+        Junior = 1000,
+        Middle = 2000,
+        Senior = 3000
+    }
+
     public class Employee
     {
         public string FirstName { get; set; }
@@ -34,6 +53,12 @@ namespace HelloWorld
 
         public bool IsAwarded;
 
+        public int? Salary;
+
+        public Level? Level;
+
+        public int FeedbackScore;
+
         public bool CanBeAwarded(int month)
         {
             var achievement = AchievementsByMonth[month];
@@ -44,6 +69,55 @@ namespace HelloWorld
             }
             return false;
         }
+
+        public void PrintGreeting(string firstName, string lastName = "", Title? title = null)
+        {
+            if (lastName != null && title.HasValue == true)
+            {
+                Debug.WriteLine($"Hello, {title.Value} {firstName} {lastName}!");
+            }
+            else if (lastName != null && title.HasValue == false)
+            {
+                Debug.WriteLine($"Hello, {firstName} {lastName}!");
+            }
+            else if (lastName == null && title.HasValue == false)
+            {
+                Debug.WriteLine($"Hello, {firstName}!");
+            }
+        }
+
+        public void DoOffer(Employee employee)
+        {
+            if (employee.Level.HasValue == true && employee.Salary.HasValue == true)
+            {
+                Debug.WriteLine($"The offer was made for {employee.Level.Value} level with the salary = {employee.Salary}"); 
+            }
+            else if (employee.Level.HasValue == true && employee.Salary.HasValue == false)
+            {
+                Debug.WriteLine($"The offer was made for {employee.Level.Value} level with the salary = {(int)employee.Level.Value}");
+            }
+            else if (employee.Level.HasValue == false && employee.Salary.HasValue == true)
+            {
+                if (employee.Salary > 3000)
+                {
+                    employee.Level = HelloWorld.Level.Senior;
+                    Debug.WriteLine($"The offer was made for {employee.Level.Value} level with the salary = {employee.Salary}");
+                }
+                else if (employee.Salary < 3000 && employee.Salary > 1000)
+                {
+                    employee.Level = HelloWorld.Level.Middle;
+                    Debug.WriteLine($"The offer was made for {employee.Level.Value} level with the salary = {employee.Salary}");
+                }
+                else if (employee.Salary < 1000)
+                {
+                    employee.Level = HelloWorld.Level.Junior;
+                    Debug.WriteLine($"The offer was made for {employee.Level.Value} level with the salary = {employee.Salary}");
+                }
+            }
+                    
+
+        }
+
 
     }
     
---
+++ NullableTypeCases.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+    public class TaxiDriver
+    {
+        public int SleepHours { get; set; }
+
+
+        public TaxiDriver() { }
+
+
+
+        public int CompareAndStoreBiggerValue(int sleepHours, int? workHours)
+        {
+            if (Nullable.Compare<int>(sleepHours, workHours) < 0)
+            {
+
+                return workHours ?? sleepHours;
+            }
+            else
+                return sleepHours;
+
+        }
+
+        public void AddingHours(out int sleepHours, out int workHours, out int total)
+        {
+            sleepHours = 9;
+            workHours = 8;
+            total = sleepHours + workHours;
+        }
+      
+    }
+}
